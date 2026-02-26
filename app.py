29dc42dd21b12c5030e3e04aa77949b0fc0f8463
SleptSkir <sleptskir@gmail.com>
2026-02-26 18:19:15 +0900

    Revert "feat: implement laureates API endpoints"
    This reverts commit 4b72f247f2970f69f16828ebb295279dfc71c79d.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 from flask import Flask, abort, jsonify, request
-from flask_restx import Api
+from flask_restx import Api  # type: ignore
 
 PAGE_SIZE = 25
 
@@ -10,8 +10,7 @@ api = Api(app)
 with open('awards.json', encoding='utf-8') as f:
     awards = json.load(f)
 
-with open('laureats.json', encoding='utf-8') as f:
-    laureates = json.load(f)
+# TODO: Добавить код для чтения лауреатов из файла
 
 
 @app.route("/api/v1/awards/")
@@ -39,26 +38,7 @@ def award_object(pk):
         abort(404)
 
 
-@app.route("/api/v1/laureates/")
-def laureates_list():
-    try:
-        p = int(request.args.get('p', 0))
-        if p < 0:
-            raise ValueError
-    except ValueError:
-        return abort(400)
-    page = laureates[p * 25:(p + 1) * 25]
-    return jsonify({
-        'page': p,
-        'count_on_page': PAGE_SIZE,
-        'total': len(laureates),
-        'items': page,
-    })
+# TODO: Добавить код для получения списка лауреатов
 
 
-@app.route("/api/v1/laureate/<int:pk>/")
-def laureate_object(pk):
-    if 0 <= pk < len(laureates):
-        return jsonify(laureates[pk])
-    else:
-        abort(404)
+# TODO: Добавить код для получения лауреата по индексу
